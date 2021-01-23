64690a9845bca8b80c2ee59420d13788380c7ccf
zeroday0619 <zeroday0619@kakao.com>
2021-01-23 17:43:17 +0900

    feat: update

--- app/twitch.py
+++ app/twitch.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from youtube_dl.YoutubeDL import YoutubeDL
+from youtube_dl.utils import DownloadError
 from app.config import load_config as _config
 
 config = _config()
@@ -12,20 +13,63 @@ class TwitchDL:
     }
     source_ = YoutubeDL(params=params)
 
-    def __init__(self) -> None:
-        self.source: YoutubeDL = self.source_
+    def __init__(self, user_name: str) -> None:
+        """
 
-    def get_stream_url(self, user_name: str) -> Dict[str, str]:
-        """Get twitch live stream url
-
-        :param user_name: Twitch streamer id
+        :param user_name: twitch streamer id
         :type user_name: str
-        :return: stream url
-        :rtype: Dict[str]
+        """
+        self.source: YoutubeDL = self.source_
+        self.user_name = user_name
+
+    def get_source(self) -> dict:
+        """
+
+        :return: twitch source
+        :rtype: dict
         """
         source: dict = self.source.extract_info(
-            url=f"https://twitch.tv/{user_name}", download=False
+            url=f"https://twitch.tv/{self.user_name}", download=False,
         )
+        return source
+
+    def is_live(self) -> bool:
+        """Live check
+
+        :return: live status
+        :rtype: bool
+        """
+        try:
+            source: dict = self.get_source()
+            status = source.get("is_live")
+            return status
+        except DownloadError:
+            return False
+
+    def get_stream_information(self) -> dict:
+        """get stream information
+
+        :return: stream info
+        :rtype: Dict[str, str, str, str, str, float]
+        """
+        source: dict = self.get_source()
+        info = {
+            "streamer_uuid": source.get("id"),
+            "streamer_id": source.get("display_id"),
+            "streamer_name": source.get("uploader"),
+            "title": source.get("title"),
+            "description": source.get("description"),
+            "timestamp": source.get("timestamp"),
+        }
+        return info
+
+    def get_stream_url(self) -> dict:
+        """Get twitch live stream url
+
+        :return: stream url
+        :rtype: Dict[str, str]
+        """
+        source = self.get_source()
         stream_url = [
             {
                 "url": i.get("manifest_url")
